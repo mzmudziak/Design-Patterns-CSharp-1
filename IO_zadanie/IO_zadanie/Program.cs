f5ab08ebbd5dcd239aa9c9bdb9e5199dbae80b21
xordan <mzmudziak@gmail.com>
2016-04-03 17:18:00 +0200

    Class hierarchy cleanup

--- IO_zadanie/IO_zadanie/Program.cs
+++ IO_zadanie/IO_zadanie/Program.cs
@@ -9,78 +9,7 @@ using System.IO;
 
 namespace IO_zadanie
 {
-					class FileReader
-					{
-										public static List<string> read(string source)
-										{
-															/*
-															@param source source filename
-															*/
-															List<string> strings = new List<string>();
-															String line;
-															try
-															{
-																				StreamReader sr = new StreamReader(source);
-																				while ((line = sr.ReadLine()) != null)
-																				{
-																									strings.Add(line);
-																				}
-																				sr.Close();
-																			
-
-															}
-															catch (IOException e)
-															{
-																				Console.WriteLine("File couldn't be open");
-																				Console.WriteLine(e.Message);
-															}
-															return strings;
-
-										}
-					}
-
-					class Currency
-					{
-
-
-					}
-
-					class Price
-					{
-
-					}
-
-					class Item
-					{
-
-					}
-
-					class ItemPack
-					{
-
-					}
-
-					class Order
-					{
-
-					}
-
-					class Customer
-					{
-
-					}
-
-					class CustomersAggregate
-					{
-
-					}
-
-					class OrdersAggregate
-					{
-
-					}
-
-					class Program
+						class Program
 					{
 										static void Main(string[] args)
 										{
