d339d8d77aa9494a99e553bae6ce44598dc7a03d
xordan <mzmudziak@gmail.com>
2016-04-27 15:39:25 +0200

    Fixed indent

--- Zad2 - Dekorator konkurs/Zad2 - Dekorator konkurs/Program.cs
+++ Zad2 - Dekorator konkurs/Zad2 - Dekorator konkurs/Program.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 namespace Zad2___Dekorator_konkurs
 {
-					class Program
-					{
-										static void Main(string[] args)
-										{
-											IContest contest = new ContestDecoratorFLZero(new ContestDecoratorMoreThanSeven(new ContestDecoratorOdd(new Contest())));
-											contest.Play();
-										}
-					}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IContest contest =
+				new ContestDecoratorFLZero(new ContestDecoratorMoreThanSeven(new ContestDecoratorOdd(new Contest())));
+			contest.Play();
+		}
+	}
 }
